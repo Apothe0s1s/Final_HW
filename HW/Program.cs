70f2f41131875a14a9ebab735b8df436ccb7a119
Apothe0s1s <118425534+Apothe0s1s@users.noreply.github.com>
2022-12-31 01:57:34 +0300

    Add_Code

--- HW/Program.cs
+++ HW/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо ровна 3 символа.
+
+string[] A = { "Happy", "New", "Year", "to", "you"};
+List<string> B = new List<string>();
+
+for (int i = 0; i < A.Length; i++)
+    if (A[i].Length <= 3)
+    {
+        B.Add(A[i]);
+    }
+
+Console.WriteLine("[{0}]", string.Join(",", B));
